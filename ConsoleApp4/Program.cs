bd844141e60722d8b276ed44a375f4d80e7a6d28
Negar <Negar@DESKTOP-9DQA4AS>
2023-11-25 23:53:12 +0330

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Getting user input
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+
+        Console.Write("Please enter your age: ");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Please enter your gender (Male/Female): ");
+        string gender = Console.ReadLine();
+
+        Console.Write("Please enter your height in centimeters: ");
+        int height = Convert.ToInt32(Console.ReadLine());
+
+        // Determining height status
+        string heightStatus;
+        if (height > 165)
+        {
+            heightStatus = "tall";
+        }
+        else
+        {
+            heightStatus = "short";
+        }
+
+        // Outputting the result
+        string title = gender.ToLower() == "male" ? "Mr." : "Ms.";
+        Console.WriteLine($"{title} {name}, your height is {height} cm, so you are considered {heightStatus}.");
+    }
+}
